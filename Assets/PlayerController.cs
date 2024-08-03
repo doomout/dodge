b8395573408d4c53193c8e48ee69e6bdad655f92
khg <winquake3@hanmail.net>
2024-08-03 18:17:18 +0900

    플레이어 이동 코드 개선
    입력 받는 방법 변경

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,37 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody playerRigidbody; //이동에 사용할 리지바디 컴포넌트
+    private Rigidbody playerRigidbody; //이동에 사용할 리지바디 컴포넌트
     public float speed = 8f; //이동 속력
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            //위쪽 방향키 입력 감지된 경우 z방향으로 힘주기
-            playerRigidbody.AddForce(0f, 0f, speed);
-        }
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            //위쪽 방향키 입력 감지된 경우 -z방향으로 힘주기
-            playerRigidbody.AddForce(0f, 0f, -speed);
-        }
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            //위쪽 방향키 입력 감지된 경우 x방향으로 힘주기
-            playerRigidbody.AddForce(speed, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            //위쪽 방향키 입력 감지된 경우 -x방향으로 힘주기
-            playerRigidbody.AddForce(-speed, 0f, 0f );
-        }
+        //수평축과 수직축의 입력값을 감지하여 저장
+        float xInput = Input.GetAxis("Horizontal"); //수평
+        float zInput = Input.GetAxis("Vertical"); //수직
+
+        //실제 이동 속도를 입력값과 이동 속력을 사용해 결정
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        //Vector3 속도를 (xSpeed, 0, zSpeed)로 생성
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        
+        playerRigidbody.velocity = newVelocity;
     }
 
     public void Die()
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ### 탄막 슈팅 게임
 게임 엔진 : 유니티 2022.3.32f1  
-IDE : Microsoft Visual Studio Community 2022 (64-bit) 
-한정된 공간에 탄이 발생하고 그 탄을 최대한 오래 피하는 게임.
+IDE : Microsoft Visual Studio Community 2022 (64-bit)   
+한정된 공간에 탄이 발생하고 그 탄을 최대한 오래 피하는 게임.  
+유니티 자체에는 깃허브가 없어서 깃허브 데스크탑 사용
 
 ### 1. 게임 초기 화면
 ![Cap 2024-08-03 17-46-53-575](https://github.com/user-attachments/assets/76d3baf3-bc41-4d6a-a222-833cbf7b9b19)
